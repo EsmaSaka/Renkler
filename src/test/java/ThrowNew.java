4aff486672c18e4cbc2e5b147c577dd7cd6f5335
esma <esmasaka5@gmail.com>
2019-12-15 17:25:17 -0500

    exc

---
+++ src/test/java/ThrowNew.java
@@ -0,0 +1,7 @@
+public class ThrowNew {
+
+    public static void main(String[] args) {
+      //  throw new RuntimeException();
+         throw new IllegalArgumentException("invalid input");
+    }
+}

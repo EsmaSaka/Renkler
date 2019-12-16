51acfa042b3d78d101d51365693ed75490d2ada1
esma <esmasaka5@gmail.com>
2019-12-15 19:14:56 -0500

    throw

---
+++ src/test/java/Citizen.java
@@ -0,0 +1,30 @@
+public class Citizen {
+    private  String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Citizen(String  name, int age) {
+        super();
+        this.name=name;
+        this.age=age;
+    }
+// go to throw new class
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/ThrowNew.java
+++ src/test/java/ThrowNew.java
@@ -1,7 +1,21 @@
 public class ThrowNew {
+    public  static void vote(Citizen citizen){
+        if (citizen.getAge()<18){
+            throw new ArithmeticException(citizen.getName()+" is not eligible for voting");
+        }
+            System.out.println(citizen.getName()+" is eligible for voting");
+
+    }
 
     public static void main(String[] args) {
       //  throw new RuntimeException();
-         throw new IllegalArgumentException("invalid input");
+       //  throw new IllegalArgumentException("invalid input");
+        vote(new Citizen("esma",35));
+        try {
+            vote(new Citizen("es", 17));
+        }catch (RuntimeException e){
+            System.out.println(e.getMessage());
+        }
+      //  System.out.println(new Citizen("yu",45));
     }
 }
